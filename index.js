76cbc045cc42f35e83480a6e6e5f60d2eab261f9
Isitar <luescherpascal@gmail.com>
2020-04-04 19:41:05 +0200

    running version, snake expands

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ const {App} = require('@slack/bolt');
 class Game {
     type_none = 0;
     type_cookie = 1;
-    type_cat = 2;
+    type_cat_head = 2;
+    type_cat_body = 3;
+    type_tail_up = 4;
+    type_tail_right = 5;
+    type_tail_left = 6;
+    type_tail_down = 7;
+
+    direction_right = 0;
+    direction_down = 1;
+    direction_left = 2;
+    direction_up = 3;
 
     lastMessage = null;
+    finished = false;
 
     getCharForType(type) {
         switch (type) {
@@ -16,14 +27,28 @@ class Game {
                 return ':white_square:';
             case this.type_cookie:
                 return ':cookie:';
-            case this.type_cat:
-                return ':cat:';
+            case this.type_cat_head:
+                return ':nyan-3:';
+            case this.type_cat_body:
+                return ':nyan-2:';
+            case this.type_tail_up:
+                return ':nyan-1:';
+            case this.type_tail_right:
+                return ':nyan-1:';
+            case this.type_tail_left:
+                return ':nyan-1:';
+            case this.type_tail_down:
+                return ':nyan-1:';
         }
     }
 
     field = [];
-    size = 10;
-    direction = 0;
+    size = 14;
+
+    direction = this.direction_right;
+
+    snake = [];
+    cookie = {};
 
     getFieldIndex(x, y) {
         return y * this.size + x;
@@ -36,7 +61,13 @@ class Game {
             }
         }
 
-        this.field[this.getFieldIndex(this.size / 2, this.size / 2)] = this.type_cat;
+        const head = {x: this.size / 2, y: this.size / 2};
+
+        this.snake.unshift({x: head.x - 1, y: head.y});
+        this.snake.unshift({x: head.x, y: head.y});
+        this.direction_right;
+
+        this.newCookie();
     }
 
     printField() {
@@ -48,6 +79,59 @@ class Game {
             retVal += "\r\n";
         }
         return retVal;
+    }
+
+    newCookie() {
+        this.cookie = {x: Math.floor(Math.random() * (this.size - 1)), y: Math.floor(Math.random() * (this.size - 1))};
+    }
+
+    tick() {
+        const newHead = {x: this.snake[0].x, y: this.snake[0].y};
+
+        switch (this.direction) {
+            case this.direction_up:
+                newHead.y--;
+                break;
+            case this.direction_right:
+                newHead.x++;
+                break;
+            case this.direction_left:
+                newHead.x--;
+                break;
+            case this.direction_down:
+                newHead.y++;
+                break;
+        }
+        this.snake.unshift(newHead);
+
+        if (!(newHead.x === this.cookie.x && newHead.y === this.cookie.y)) {
+            this.snake.pop();
+        } else {
+            this.newCookie();
+        }
+
+        //redraw field
+        for (let x = 0; x < this.size; x++) {
+            for (let y = 0; y < this.size; y++) {
+                this.field[this.getFieldIndex(x, y)] = this.type_none;
+            }
+        }
+        // draw snake
+        for (let i = 0; i < this.snake.length; i++) {
+            const f = this.getFieldIndex(this.snake[i].x, this.snake[i].y);
+            if (0 === i) {
+                this.field[f] = this.type_cat_head;
+            } else if (1 === i) {
+                this.field[f] = this.type_cat_body;
+            } else {
+                this.field[f] = this.type_tail_right;
+            }
+        }
+
+        // draw cookie
+        console.log(this.cookie);
+        this.field[this.getFieldIndex(this.cookie.x, this.cookie.y)] = this.type_cookie;
+
     }
 }
 
@@ -71,6 +155,43 @@ app.message('game', async ({message, say}) => {
     await say('started game');
 });
 
+app.message('left', async ({message, say}) => {
+    const game = games[message.channel];
+    if (undefined === game) {
+        return;
+    }
+    game.direction = game.direction_left;
+});
+app.message('right', async ({message, say}) => {
+    const game = games[message.channel];
+    if (undefined === game) {
+        return;
+    }
+    game.direction = game.direction_right;
+});
+app.message('down', async ({message, say}) => {
+    const game = games[message.channel];
+    if (undefined === game) {
+        return;
+    }
+    game.direction = game.direction_down;
+});
+app.message('up', async ({message, say}) => {
+    const game = games[message.channel];
+    if (undefined === game) {
+        return;
+    }
+    game.direction = game.direction_up;
+});
+
+app.message('quit', async ({message, say}) => {
+    const game = games[message.channel];
+    if (undefined === game) {
+        return;
+    }
+    game.finished = true;
+});
+
 (async () => {
     // Start your app
     await app.start(process.env.PORT || 3000)
@@ -88,9 +209,6 @@ app.message('game', async ({message, say}) => {
                     }
                 ).catch(console.error);
         });
-
-
-    console.log('⚡️ Bolt app is running!');
 })();
 
 // game loop
@@ -99,20 +217,25 @@ app.message('game', async ({message, say}) => {
         await Sleep(1000);
         for (let channelToken in games) {
             const game = games[channelToken];
+            if (game.finished) {
+                delete games[channelToken];
+                continue;
+            }
+            game.tick();
+            const response = await app.client.chat.postMessage({
+                token: process.env.SLACK_BOT_TOKEN,
+                channel: channelToken,
+                text: game.printField()
+            });
             if (null !== game.lastMessage) {
-                console.log('last message was', game.lastMessage)
                 await app.client.chat.delete({
                     token: process.env.SLACK_BOT_TOKEN,
                     channel: channelToken,
                     ts: game.lastMessage
                 });
             }
-            const response = await app.client.chat.postMessage({
-                token: process.env.SLACK_BOT_TOKEN,
-                channel: channelToken,
-                text: game.printField()
-            });
             game.lastMessage = response.message.ts;
+
         }
     }
 })();
